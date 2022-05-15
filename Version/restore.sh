353abdd4a5d5b875e3b0bfff5b6f1a63da44cc85
Jorge <777570@unizar.es>
2022-05-15 21:56:53 +0200

    cambios

--- Version/restore.sh
+++ Version/restore.sh
@@ -1,4 +1,4 @@
 #!/bin/bash
-echo "Se va a restaurar la version $1 del fichero Texto1.txt"
-./version restore Texto1.txt --version $1
-echo "El fichero Texto1.txt esta ahora en la version $1"
+echo "Se va a restaurar la version $1 del fichero $2"
+./version restore $2 --version $1
+echo "El fichero $2 esta ahora en la version $1"
